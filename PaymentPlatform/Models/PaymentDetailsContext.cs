09b73ffd44a8b8c469aeb45bf2fef0f230056bac
Houssnou <hosni.moubarak@gmail.com>
2019-06-28 15:21:46 -0400

    project init

---
+++ PaymentPlatform/Models/PaymentDetailsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentPlatform.Models
+{
+    public class PaymentDetailsContext:DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options):base(options)
+        {
+                
+        }
+    }
+}
